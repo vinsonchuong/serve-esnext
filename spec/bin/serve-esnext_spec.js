4bff4e4db2ca45d6bd46d143632bf7af1f6a131f
Vinson Chuong <vinsonchuong@gmail.com>
2016-05-22 20:42:40 -0700

    Failing test to drive out phantomjs-promise-es6

--- spec/bin/serve-esnext_spec.js
+++ spec/bin/serve-esnext_spec.js
@@ -1,8 +1,41 @@
+import {childProcess} from 'node-promise-es6';
 import Directory from 'directory-helpers';
+import PhantomJS from 'phantomjs-promise-es6';
+
+class Project extends Directory {
+  start() {
+    return this.spawn('npm', ['start']);
+  }
+
+  async stop() {
+    try {
+      const {stdout: serverPid} = await childProcess.exec("pgrep -f 'node.*serve-esnext$'");
+      await childProcess.exec(`kill ${serverPid}`);
+    } catch (error) {
+      if (error.message.indexOf("pgrep -f 'node.*serve-esnext$'") === -1) {
+        throw error;
+      }
+    }
+  }
+}
 
 describe('serve-esnext', () => {
-  it('outputs "3...2...1...Hello World!"', async () => {
-    const directory = new Directory('.');
-    expect(await directory.exec('serve-esnext')).toBe('Hello World!');
+  afterEach(async () => {
+    const project = new Project('project');
+    await project.stop();
+    await project.remove();
+  });
+
+  it('serves ES.next to a browser', async () => {
+    const project = new Project('project');
+
+    const server = project.start();
+    await server.filter((output) => output.match(/Listening/));
+
+    const browser = new PhantomJS();
+    await browser.open('http://localhost:8080');
+    expect(browser.evaluate((window) =>
+      window.document.querySelector('#container').textContent
+    )).toBe('Hello World!');
   });
 });
